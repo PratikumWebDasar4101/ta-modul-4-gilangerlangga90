3a7a02e12ebbafb6ef237f45e26f6c877e5caf30
gilangerlangga90 <42725308+gilangerlangga90@users.noreply.github.com>
2018-09-26 22:37:43 +0700

    Update proses_belanja.php

--- proses_belanja.php
+++ proses_belanja.php
@@ -1,3 +1,25 @@
 <?php
+session_start();
+
+$user = $_SESSION["user"];
+$nama = $_POST["nama"];
+$barang = $_POST["barang"];
+$pengiriman = $_POST["pengiriman"];
+$alamat = $_POST["alamat"];
+$harga = 0;
+$ongkir = 0;
+
+	if($pengiriman == "JNE")
+		$ongkir = 22000;
+	if($pengiriman == "TIKI")
+		$ongkir = 17000;
+	if($pengiriman == "Pos")
+		$ongkir = 15000;
+
+	function total($a, $b) {
+		return $a + $b;
+	}
+
+	include("struk_belanja.php");
 
 ?>
