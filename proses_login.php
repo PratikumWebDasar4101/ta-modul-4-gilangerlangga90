9d8774fe97bab8f73ae161242ff41a577d5b24be
gilangerlangga90 <42725308+gilangerlangga90@users.noreply.github.com>
2018-09-26 22:37:13 +0700

    Update proses_login.php

--- proses_login.php
+++ proses_login.php
@@ -1,3 +1,17 @@
 <?php
+session_start();
+
+$user = array(
+				"username" => "user",
+				"password" => "maubelanja"
+			);
+
+if ($user["username"] == $_POST["username"] && $user["password"] == $_POST["password"]) {
+	$_SESSION["username"] = $user["username"];
+	$_SESSION["user"] = $user;
+	header("Location: halaman_belanja.php");
+}else {
+	header("Location: login_belanja.php");
+}
 
 ?>
