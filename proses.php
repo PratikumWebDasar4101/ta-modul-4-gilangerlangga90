b98806450f6b9a547300c6398c29512e66db8104
gilangerlangga90 <42725308+gilangerlangga90@users.noreply.github.com>
2018-09-26 22:33:10 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,21 @@
 <?php
+session_start();
 
+$nama_file = $_FILES["foto"]["name"];
+$nama_file_tmp = $_FILES["foto"]["tmp_name"];
+$dir = "upload/";
+$target_file = $dir.$nama_file;
+move_uploaded_file($nama_file_tmp, $target_file);
+
+$user = array(
+	"nama" => $_POST["nama"],
+	"foto" => $target_file,
+	"hobi" => $_POST["hobi"],
+	"genre_film" => $_POST["genre_film"],
+	"travelling" => $_POST["travelling"]
+	);
+
+$_SESSION["user"] = $user;
+
+include("halamanakhir.php")
 ?>
